016769963f1d380d1fc2572b871024e36e4e4c5a
Igor Gulewski <igorgulewski0@gmail.com>
2025-12-27 19:33:13 +0100

    Fishing Bot

---
+++ FishBot.py
@@ -0,0 +1,48 @@
+from pycaw.pycaw import AudioUtilities as AU, IAudioMeterInformation
+from time import sleep
+import keyboard
+import pyautogui
+aplikacje = AU.GetAllSessions()
+def zarzut():
+    sleep(0.1)
+    pyautogui.mouseDown()
+    sleep(0.4)
+    pyautogui.mouseUp()
+    print("Zarzut")
+def zlow():
+    pyautogui.mouseDown()
+    sleep(0.05)
+    pyautogui.mouseUp()
+    print("Złowiona")
+
+for apl in aplikacje:
+    print(apl)
+    if apl.Process!=None:
+        if apl.Process.name()=="crueltysquad.exe": #Tutaj jaka aplikacja jest monitorowana
+            szuk=apl
+            break
+else:
+    print("Nie znaleziono procesu L")
+    exit(0)
+
+miernik = szuk._ctl.QueryInterface(IAudioMeterInformation)
+print("Aby zaczac wcisnij q (by skonczyc tez btw)")
+while True:          
+    sleep(0.1)
+    if keyboard.is_pressed('q'):
+        print("Start Rybienia za 2 sekundy (puść guzik)")
+        sleep(2)
+        break
+zarzut()
+while True:
+    glos = miernik.GetPeakValue()
+    print(f"Poziom glosnosci: {glos}")
+    sleep(0.03)
+    if keyboard.is_pressed('q'):
+        print("Koniec Rybienia")
+        break
+    if glos>0.007 and glos<0.009:
+        print("Branie...")
+        zlow()
+        sleep(1)
+        zarzut()
---
+++ README.txt
@@ -0,0 +1,4 @@
+While having rod in hand and next to pond press q to start fishing:
+Bot works by analyzing ingame sound, so loud sound in game may also be interpreted as catching fish.
+Bot works on (my ingame settings) so u may to adjust FishBot.py glos>0.007 and glos<0.009 to your need (muting music is recommended).
+To terminate press q again.
